482fd58536303fc90fbb341df16109f15ed7e154
Anita <anitashah92@gmail.com>
2018-06-21 09:11:03 -0700

    added time

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,6 +16,7 @@
        player.playPause(nextSong);
      });
 
+
      $('button#previous').on('click', function() {
           if (player.playState !== 'playing') { return; }
 
@@ -28,5 +29,18 @@
         });
 
 
+        $('#time-control input').on('input', function (event) {
+          player.skipTo(event.target.value);
+        });
+
+
+        setInterval( () => {
+          if (player.playState !== 'playing') { return; }
+          const currentTime = player.getTime();
+          const duration = player.getDuration();
+          const percent = (currentTime / duration) * 100;
+          $('#time-control .current-time').text( currentTime );
+          $('#time-control input').val(percent);
+        }, 1000);
 
 }
